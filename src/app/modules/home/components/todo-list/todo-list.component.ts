e6ec56e610148a9ea8c27e2e69f82b1cd3a80b9d
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2022-07-13 11:52:59 -0300

    adicionando funcionalidade para adicionar item e validacoes

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -8,13 +8,14 @@ import { TaskList } from '../../model/task-list';
 })
 export class TodoListComponent implements OnInit {
 
-  public taskList: Array<TaskList> = [
-    {task: "Test", checked: true},
-    {task: "Test 2", checked: false}
-  ]
+  public taskList: Array<TaskList> = []
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public setEmitTaskList(event: string) {
+    this.taskList.push({task: event, checked: false})
   }
 
   public deleteItemTaskList(event: number) {
